1849bda944725d59df2e93bef1c9ce3f7de0fd7d
DynoDojo <doug_ohara@hotmail.com>
2017-03-15 14:39:06 -0600

    Delete _Update DJO-1.bat

--- _Update DJO-1.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-robocopy "P:\Games\EverQuest\MacroQuest\Macros" "c:\program files\MQ2\Macros"
-rem robocopy *.inc "c:\program files\MQ2\Macros"
-rem robocopy *.ini "c:\program files\MQ2\Macros"
-
-Pause
