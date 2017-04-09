ea879a7522dd3166ddcb6c522e18b00c2c5966e2
Doug O'Hara <Doug O'Hara>
2017-04-09 13:50:46 -0600

    Broke out Wizzy Section

--- __RobocopyALL.bat
+++ __RobocopyALL.bat
@@ -4,10 +4,12 @@ rem Get the latest files to the PCs:
 robocopy "\\nas1\public$\Games\EverQuest\MacroQuest\Macros" "C:\Program Files\MQ2\Macros" /XO
 robocopy "\\nas1\public$\Games\EverQuest\MacroQuest\Macros" "C:\Users\doug_\Google Drive\Macros" /XO
 robocopy "\\nas1\public$\Games\EverQuest\MacroQuest\Macros" "\\djo-2\MQ2$\Macros" /XO
+robocopy "\\nas1\public$\Games\EverQuest\MacroQuest\Macros" "\\djo-3\MQ2$\Macros" /XO
 
 rem Collect any .ini file updates from the PCs:
 robocopy "C:\Program Files\MQ2\Macros" "\\nas1\public$\Games\EverQuest\MacroQuest\Macros" iLoot.ini /XO
 robocopy "\\djo-2\MQ2$\Macros" "\\nas1\public$\Games\EverQuest\MacroQuest\Macros" iLoot.ini /XO
+robocopy "\\djo-3\MQ2$\Macros" "\\nas1\public$\Games\EverQuest\MacroQuest\Macros" iLoot.ini /XO
 
 rem robocopy /?
 rem Pause
